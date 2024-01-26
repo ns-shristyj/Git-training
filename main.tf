f60d04d73a087c8b7b7a7353bafd3f9eb6925fa1
Conor Quinlan <cquinlan@netskope.com>
2024-01-26 15:31:03 -0600

    Changes to Terraform.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 // For more about Terraform using EC2: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-
 // Maybe one of the github actions could be terraform init so it can download AWS Ec2?
 
 // Terraform block used to configure some high-level behaviors of Terraform
@@ -10,6 +9,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  required_version = ">=0.14.9"
+
+  backend "s3" {
+    bucket =  "[Remote State Bucket Name]"
+    key =     "[Remote State Bucket Key]"
+    region =  "ap-northeast-2"
+  }
 }
 
 // Configure the AWS Provider with Credentials
@@ -19,11 +25,7 @@ provider "aws" {
     secret_key = ""                             // Needs to be changed
 }
 
-// How to create resources with a provider:
-// resource "<provider>_<resource_type>" "name" {
-//      config options
-//      key = "value"
-// }
+
 resource "aws_instance" "my-first-server" {
   ami           = data.aws_ami.ubuntu.id        // Needs to be changed, might be something like "ami-239842dafklfh832"
   instance_type = "t3.micro"                    // Needs to be changed
