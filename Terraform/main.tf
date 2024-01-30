ee88c3b93e3dfd3872d192dd79e2d12eff99c4b6
Conor Quinlan <cquinlan@netskope.com>
2024-01-30 13:00:04 -0600

    Terraform Ec2 VPC Update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -130,7 +130,7 @@ resource "aws_instance" "cobalt" {
   ami               = "ami-0f3a440bbcff3d043"
   instance_type     = "t2.medium"
   availability_zone = "ap-northeast-2a"
-  key_name          = "conor-intern-key-pair"       // Needs to be Checked / Auth
+  key_name          = "conor-intern-key-pair"       // Needs to be Checked
 
   network_interface {
     device_index          = 0
