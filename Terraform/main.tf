832ef262d032f63e8bae2e7f7fde7fc13a9a41e5
Conor Quinlan <cquinlan@netskope.com>
2024-01-30 16:06:05 -0600

    Terraform Ec2 SG Updates

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "allow_web" {
 resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4-1" {
   description       = "HTTPS"
   security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = aws_vpc.AWS-VPC.cidr_block                   // this is responsible for which IP addresses can come into the Network
+  cidr_ipv4         = ["104.53.62.2/32", "24.206.70.11/32"]                   // this is responsible for which IP addresses can come into the Network
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443 
@@ -88,10 +88,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4-1" {
 resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4-2" {
   description       = "ALT HTTPS"
   security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = aws_vpc.AWS-VPC.cidr_block                   // this is responsible for which IP addresses can come into the Network
+  cidr_ipv4         = ["104.53.62.2/32", "24.206.70.11/32"]                  // this is responsible for which IP addresses can come into the Network
   from_port         = 8443
   ip_protocol       = "tcp"
   to_port           = 8443 
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4-3" {
+  description       = "ALT HTTPS"
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = ["104.53.62.2/32", "24.206.70.11/32"]                  // this is responsible for which IP addresses can come into the Network
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22 
 }
 
 // Egress - Security Group
