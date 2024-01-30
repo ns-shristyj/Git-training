9bbf87f714b5609f9a0bade11ac173a8b6984c1d
Conor Quinlan <cquinlan@netskope.com>
2024-01-30 15:36:19 -0600

    Terraform Ec2 S3 Updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,20 +23,9 @@ terraform {
   }
 }
 
-variable access_key {
-  type = string
-  sensitive = true
-}
-variable secret_key {
-  type = string
-  sensitive = true
-}
-
 // Configure the AWS Provider with Credentials
 provider "aws" {
     region = "ap-northeast-2"
-    access_key = var.access_key
-    secret_key = var.secret_key
 }
 
 // Create the VPC
