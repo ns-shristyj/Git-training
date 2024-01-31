d6390b4e3ab7f2f2dfb0fa3405c212d78232dd23
Conor Quinlan <cquinlan@netskope.com>
2024-01-31 10:47:02 -0600

    Terraform Ec2 Server Updates

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,15 +10,18 @@ terraform {
   required_version = ">=0.14.9"
 
   backend "s3" {
-    bucket =  "seceng-terraform-state-storage"
-    key =     "terraform.tfstate"
-    region =  "us-west-1"
+    bucket    = "seceng-terraform-state-storage"
+    key       = "terraform.tfstate"
+    region    = "us-west-1"
   }
 }
 
 // Configure the AWS Provider with Credentials
 provider "aws" {
     region = "us-west-1"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
 }
 
 // Create the VPC
