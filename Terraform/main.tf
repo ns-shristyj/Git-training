45d41c47771c4e4fd039bc56296a91db58c28f5b
Conor Quinlan <cquinlan@netskope.com>
2024-01-31 12:06:39 -0600

    Ansible Changes

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -19,9 +19,6 @@ terraform {
 // Configure the AWS Provider with Credentials
 provider "aws" {
     region = "us-west-1"
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
 }
 
 // Create the VPC
