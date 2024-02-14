6902a141d46879db4324452d978162b526427951
Conor Quinlan <cquinlan@netskope.com>
2024-02-14 14:28:43 -0600

    More change

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -67,11 +67,11 @@ resource "aws_route_table_association" "a" {
 }
 
 // Create a Network Interface with an IP in the subnet
-# resource "aws_network_interface" "net_face" {
-#   subnet_id       = aws_subnet.AWS-SUBNET-1.id
-#   private_ips     = [aws_eip.EIP-1.public_ip]
-#   security_groups = [aws_security_group.allow_web.id]
-# }
+resource "aws_network_interface" "net_face" {
+  subnet_id       = aws_subnet.AWS-SUBNET-1.id
+  private_ips     = [aws_eip.EIP-1.public_ip]
+  security_groups = [aws_security_group.allow_web.id]
+}
 // Create a Security Group
 resource "aws_security_group" "allow_web" {
   name        = "allow_web_traffic"
@@ -144,10 +144,10 @@ resource "aws_security_group" "allow_web" {
 }
 
 // Assigning a Elastic IP to the Network Interface
-# resource "aws_eip" "EIP-1" {
-#   network_interface         = "${aws_network_interface.net_face.id}"
-#   associate_with_private_ip = "10.0.1.50"
-# }
+resource "aws_eip" "EIP-1" {
+  network_interface         = "${aws_network_interface.net_face.id}"
+  associate_with_private_ip = "10.0.1.50"
+}
 
 // Create Ubuntu Server and install Cobalt Strike
 resource "aws_instance" "cobalt" {
@@ -155,12 +155,13 @@ resource "aws_instance" "cobalt" {
   instance_type     = "t2.medium"
   availability_zone = "us-west-1b"
   key_name          = "seceng-intern-key-pair"
+  associate_public_ip_address = aws_eip.EIP-1.public_ip
 
-  # network_interface {
-  #   device_index          = 0
-  #   network_interface_id  = aws_network_interface.net_face.id
-  #   delete_on_termination = false
-  # }
+  network_interface {
+    device_index          = 0
+    network_interface_id  = aws_network_interface.net_face.id
+    delete_on_termination = false
+  }
 }
 
 # Useful for outputting the server's public IP
