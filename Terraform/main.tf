39af29bf81bb575a60e32a664cacaf6217ccdb06
Conor Quinlan <cquinlan@netskope.com>
2024-03-25 12:28:06 -0500

    Debug mode Enabled

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "allow_web" {
                 "34.105.33.53/32", "34.83.158.21/32",
                 "35.233.206.241/32", "35.247.6.21/32",
                 "35.247.67.124/32", "35.197.82.112/32",
-                "24.206.84.11/32", "163.116.133.35"]
+                "24.206.84.11/32", "163.116.133.35/32"]
   }
   // IPv4 - Ingress Security Group dealing with Port 8443
   // The IP's are allowing GitHub Runner's and, Netskope Client
@@ -103,7 +103,7 @@ resource "aws_security_group" "allow_web" {
                 "34.105.33.53/32", "34.83.158.21/32",
                 "35.233.206.241/32", "35.247.6.21/32",
                 "35.247.67.124/32", "35.197.82.112/32",
-                "24.206.84.11/32", "163.116.133.35"]
+                "24.206.84.11/32", "163.116.133.35/32"]
   }
   // IPv4 - Ingress Security Group dealing with Port 22
   // The IP's are allowing GitHub Runner's and, Netskope Client  
@@ -131,7 +131,7 @@ resource "aws_security_group" "allow_web" {
                 "34.105.33.53/32", "34.83.158.21/32",
                 "35.233.206.241/32", "35.247.6.21/32",
                 "35.247.67.124/32", "35.197.82.112/32",
-                "24.206.84.11/32", "163.116.133.35"]
+                "24.206.84.11/32", "163.116.133.35/32"]
   }
   // IPv4 - Egress Security Group
   // This allows all IP's to leave.
