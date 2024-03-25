24416911964230f206cdbcd312c4e35cfcb00135
Conor Quinlan <cquinlan@netskope.com>
2024-03-25 12:27:13 -0500

    Debug mode Enabled

--- project-emu-develop/app.py
+++ project-emu-develop/app.py
@@ -1776,4 +1776,4 @@ if __name__ == '__main__':
     scheduler.add_job(func=syncThirdPartyTrustVendors, trigger="interval", minutes=45)
     scheduler.start()
 
-    app.run(debug=False, host='0.0.0.0', port=5000, use_reloader=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=True)
