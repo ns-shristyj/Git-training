32392b993e3f5d4cb945c448bd182a97304e765b
Tushar Singh <tsingh@netskope.com>
2024-05-21 18:27:32 +0530

    test-csv pushing

--- dump_csv.py
+++ dump_csv.py
@@ -1,19 +1,24 @@
 import os
 from google.cloud import storage
 
-def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the GCS bucket."""
+def upload_file_to_gcs(bucket_name, source_file_name):
+  
     storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
     
-    blob.upload_from_filename(source_file_name)
+   
+    bucket = storage_client.bucket(bucket_name)
+    
+   
+    blob = bucket.blob(os.path.basename(source_file_name))
+    
 
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+    blob.upload_from_filename(source_file_name)
+    
+    print(f"File {source_file_name} uploaded to {bucket_name} as {blob.name}.")
 
 if __name__ == "__main__":
-    bucket_name = os.getenv("GCS_BUCKET_NAME")
-    source_file_name = "test.csv"
-    destination_blob_name = "test.csv"
-
-    upload_to_gcs(bucket_name, source_file_name, destination_blob_name)
+   
+    bucket_name = "test-saas-bucket"
+    source_file_name = "C:\Users\TusharSingh\GIS-SecEng-Intern\test.csv"
+    
+    upload_file_to_gcs(bucket_name, source_file_name)
