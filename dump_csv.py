7bd65afccfa21a1b8eb129b7ca543cfdd4aa23be
Tushar Singh <tsingh@netskope.com>
2024-05-22 11:39:20 +0530

    check for blob

--- dump_csv.py
+++ dump_csv.py
@@ -8,14 +8,21 @@ def upload_file_to_gcs(bucket_name, source_file_name):
    
     bucket = storage_client.bucket(bucket_name)
     
-   
-    blob = bucket.blob(os.path.basename(source_file_name))
+    # blob = bucket.blob(test.csv)
+    # blob.upload_from_filename(source_file_name)
+    # print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+    # blob = bucket.blob(os.path.basename(source_file_name))
     
 
-    blob.upload_from_filename(source_file_name)
+    # blob.upload_from_filename(source_file_name)
     
-    print(f"File {source_file_name} uploaded to {bucket_name} as {blob.name}.")
-
+    # print(f"File {source_file_name} uploaded to {bucket_name} as {blob.name}.")
+    blob = bucket.blob(test.csv)
+    if blob :
+       print("exists")
+    else :
+        print("doesnot exist") 
+        
 if __name__ == "__main__":
    
     bucket_name = "test-saas-bucket"
