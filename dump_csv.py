84bb19b7013c9d910262d5dd859be506384f3f7c
Tushar Singh <tsingh@netskope.com>
2024-05-22 12:15:04 +0530

    replacing file

--- dump_csv.py
+++ dump_csv.py
@@ -1,7 +1,7 @@
 import os
 from google.cloud import storage
 
-def upload_file_to_gcs(bucket_name, source_file_name):
+def upload_file_to_gcs(bucket_name, source_file_name,destination_blob_name):
   
     storage_client = storage.Client()
     
@@ -17,15 +17,14 @@ def upload_file_to_gcs(bucket_name, source_file_name):
     # blob.upload_from_filename(source_file_name)
     
     # print(f"File {source_file_name} uploaded to {bucket_name} as {blob.name}.")
-    blob = bucket.blob(source_file_name)
-    if blob :
-       print("exists")
-    else :
-        print("doesnot exist") 
+     
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")    
 
 if __name__ == "__main__":
    
     bucket_name = "test-saas-bucket"
     source_file_name = "test.csv"
-    
-    upload_file_to_gcs(bucket_name, source_file_name)
+    destination_blob_name="test2.csv"
+    upload_file_to_gcs(bucket_name, source_file_name,destination_blob_name)
