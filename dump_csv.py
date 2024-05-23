5772b226aba0082b515bc1f7fa5e3f1636ff9d95
Tushar Singh <tsingh@netskope.com>
2024-05-23 14:13:29 +0530

    hashicorp login method 2

--- dump_csv.py
+++ dump_csv.py
@@ -4,10 +4,7 @@ from google.cloud import storage
 def upload_file_to_gcs(bucket_name, source_file_name,destination_blob_name):
   
     storage_client = storage.Client()
-    
-   
     bucket = storage_client.bucket(bucket_name)
-    
     # blob = bucket.blob(test.csv)
     # blob.upload_from_filename(source_file_name)
     # print(f"File {source_file_name} uploaded to {destination_blob_name}.")
