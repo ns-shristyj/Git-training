122750286b0ca8e962c74537bfd8fe6202b6aa8a
Conor Quinlan <cquinlan@netskope.com>
2024-06-28 12:47:10 -0700

    Updating Ticket Creation

--- censys/jira_ticketing.py
+++ censys/jira_ticketing.py
@@ -165,7 +165,8 @@ def add_attachment(issue_key):
         headers={'X-Atlassian-Token': 'no-check'}, 
         auth=auth, 
         files = {
-            "file": ('s3status.txt', open('s3status.txt', 'rb'), "application-type")
+            "file": ('S3 Bucket List', open('s3status.txt', 'rb'), "application-type"),
+            "file": ('GCP Bucket List', open('gcpSecUrls.txt', 'rb'), "application-type")
         })
     
     if response.status_code == 200:
