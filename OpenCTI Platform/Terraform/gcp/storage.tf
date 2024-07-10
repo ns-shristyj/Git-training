0a8530d2b11ec60f24922d3aead93d6165821286
Conor Quinlan <cquinlan@netskope.com>
2024-07-10 13:25:51 -0700

    Updating

--- OpenCTI Platform/Terraform/gcp/storage.tf
+++ OpenCTI Platform/Terraform/gcp/storage.tf
@@ -1,10 +1,4 @@
 # Create storage bucket and add install and connectors script to it.
-# resource "google_storage_bucket" "opencti_storage" {
-#   name          = var.storage_bucket
-#   location      = var.region
-#   force_destroy = true
-# }
-
 resource "google_storage_bucket_object" "opencti_install_script" {
   name   = local.install_script_name
   source = "../opencti_scripts/installer.sh"
