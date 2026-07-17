ef0d4875b29336c40104a31c8f1da721a35a5e78
Aryan Panikar <apanikar@netskope.com>
2026-07-17 11:25:02 +0530

    Update Strings File

--- NIC_SecEng_Task/Calculator/string_func.py
+++ NIC_SecEng_Task/Calculator/string_func.py
@@ -4,13 +4,13 @@ def reverse_string(text: str) -> str:
     return text[::-1]
 
 def capitalize_words(text: str) -> str:
-    """Capitalizes 1 letter of every word in a string."""
+    """Capitalizes 1st letter of every word in a string."""
     if not text:
         return ""
     return " ".join(word.capitalize() for word in text.split())
 
 def truncate(text: str, max_length: int) -> str:
-    """Truncates text to a specified length and appends ellipses (...) if it exceeds it."""
+    """Truncates text to specified length and appends ellipses (...) if it exceeds it."""
     if max_length <= 0:
         raise ValueError("Maximum length must be a positive integer.")
         
