90e1c10e59a3f996829837d54772b9025660a0ad
Aryan Panikar <apanikar@netskope.com>
2026-07-20 18:00:38 +0530

    Trim trailing blank lines in string_func.py

--- NIC_SecEng_Task/Calculator/string_func.py
+++ NIC_SecEng_Task/Calculator/string_func.py
@@ -16,9 +16,4 @@ def truncate(text: str, max_length: int) -> str:
         
     if len(text) <= max_length:
         return text
-    return text[:max_length] + "..."    
- 
- 
- 
- 
- 
+    return text[:max_length] + "..."
